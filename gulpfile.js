0ca917e9ec846ee4996b1f0a537da5e47f160b47
Alex <manakov@e1media.ru>
2017-07-17 15:15:09 +0700

    basic layout

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,14 @@ var gulp      	= require('gulp'), // Подключаем Gulp
 gulp.task('sass', function(){ // Создаем таск Sass
     return gulp.src('app/scss/**/*.scss') // Берем источник
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(prefix()) // Добавляем вендорные префиксы. Настройки - в package.json
-        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(gulp.dest('dist/css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
     browserSync({ // Выполняем browserSync
         server: { // Определяем параметры сервера
-            baseDir: 'app' // Директория для сервера - app
+            baseDir: 'dist' // Директория для сервера - app
         },
         notify: true // Отключаем уведомления
     });
@@ -22,7 +21,7 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 
 gulp.task('watch', ['browser-sync', 'sass'], function() {
     gulp.watch('app/scss/**/*.scss', ['sass']); // Наблюдение за sass файлами
-    gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('dist/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
     // Наблюдение за другими типами файлов
 });
 
